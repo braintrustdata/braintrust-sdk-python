cf9118ec73c8135f004195cb02b045c8e561a2b5
Barrett Pyke <barrett.pyke@braintrustdata.com>
2026-05-21 13:18:29 -0500

    remove underscore from PydanticV2Metadata and PydanticV1Metadata

--- py/src/braintrust/types/__init__.py
+++ py/src/braintrust/types/__init__.py
@@ -2,12 +2,12 @@ from collections.abc import Mapping
 from typing import Any, Protocol, TypeAlias
 
 
-class _PydanticV2Metadata(Protocol):
+class PydanticV2Metadata(Protocol):
     def model_dump(self, *, exclude_none: bool = ...) -> Mapping[str, Any]: ...
 
 
-class _PydanticV1Metadata(Protocol):
+class PydanticV1Metadata(Protocol):
     def dict(self, *, exclude_none: bool = ...) -> Mapping[str, Any]: ...
 
 
-Metadata: TypeAlias = Mapping[str, Any] | _PydanticV2Metadata | _PydanticV1Metadata
+Metadata: TypeAlias = Mapping[str, Any] | PydanticV2Metadata | PydanticV1Metadata
